03be2f7df407f99e48c4eccfbd5e4b194ca643ad
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-17 17:00:33 +0800

    adds observable

--- src/app/observable-playground/observable-playground.component.ts
+++ src/app/observable-playground/observable-playground.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observable-playground.component.css']
 })
 export class ObservablePlaygroundComponent implements OnInit {
+// create an observable
+// subscribe to it
+// unsubscribe
 
+  observable = new Observable(observer => {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.next(4);
+    observer.next(5);
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.observable.subscribe(
+      value => console.log(value),
+      error => console.log(error),
+      () => console.log('completed')
+    );
   }
 
 }
