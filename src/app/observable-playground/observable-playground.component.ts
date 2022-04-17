093f40bd771f10072da63c69a0e0b58a5c0cdfa9
alphaVariable <vikasoffice81@gmail.com>
2022-04-17 17:05:20 +0800

    1 file modified

--- src/app/observable-playground/observable-playground.component.ts
+++ src/app/observable-playground/observable-playground.component.ts
@@ -3,33 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-observable-playground',
   templateUrl: './observable-playground.component.html',
-  styleUrls: ['./observable-playground.component.css']
+  styleUrls: ['./observable-playground.component.css'],
 })
 export class ObservablePlaygroundComponent implements OnInit, OnDestroy {
-// create an observable
-// subscribe to it
-// unsubscribe
+  // create an observable
+  // subscribe to it
+  // unsubscribe
 
-  observable = new Observable(observer => {
+  observable = new Observable((observer) => {
     observer.next(1);
     observer.next(2);
-    observer.next(3);
-    observer.next(4);
-    observer.next(5);
+    observer.complete();
   });
-  
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
     this.observable.subscribe(
-      value => console.log(value),
-      error => console.log(error),
+      (value) => console.log(value),
+      (error) => console.log(error),
       () => console.log('completed')
-    );    
+    );
   }
   ngOnDestroy() {
     console.log('destroyed');
     this.observable.unsubscribe();
   }
-
 }
