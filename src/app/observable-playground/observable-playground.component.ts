a7f0c87c32b8f7fc824bd9a1831e717a69e6f55a
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-17 17:26:44 +0800

    adds observable

--- src/app/observable-playground/observable-playground.component.ts
+++ src/app/observable-playground/observable-playground.component.ts
@@ -7,7 +7,22 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./observable-playground.component.css'],
 })
 export class ObservablePlaygroundComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    const observable$ = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+    observable$.subscribe(
+      value => console.log(value),
+      error => console.log(error),
+      () => console.log('completed')
+    );
+  }
 
   ngOnInit(): void {}
 }
