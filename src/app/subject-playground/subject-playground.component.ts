59e655366b8236b3652454654f34accf0b818600
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-17 17:49:49 +0800

    adds subject pg

--- src/app/subject-playground/subject-playground.component.ts
+++ src/app/subject-playground/subject-playground.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 @Component({
   selector: 'app-subject-playground',
   templateUrl: './subject-playground.component.html',
   styleUrls: ['./subject-playground.component.css']
 })
-export class SubjectPlaygroundComponent implements OnInit {
+export class SubjectPlaygroundComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+  }
 }
