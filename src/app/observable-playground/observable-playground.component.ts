4e321302b8b754c6a7f36868a00d4a4c55ec01a2
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-17 17:17:15 +0800

    adds observable

--- src/app/observable-playground/observable-playground.component.ts
+++ src/app/observable-playground/observable-playground.component.ts
@@ -7,27 +7,13 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./observable-playground.component.css'],
 })
 export class ObservablePlaygroundComponent implements OnInit, OnDestroy {
-  // create an observable
-  // subscribe to it
-  // unsubscribe
 
-  observable = new Observable((observer) => {
-    observer.next(1);
-    observer.next(2);
-    observer.complete();
-  });
 
   constructor() {}
 
   ngOnInit(): void {
-    this.observable.subscribe(
-      (value) => console.log(value),
-      (error) => console.log(error),
-      () => console.log('completed')
-    );
   }
   ngOnDestroy() {
     console.log('destroyed');
-    this.observable.unsubscribe();
   }
 }
